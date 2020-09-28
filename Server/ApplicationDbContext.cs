d68c0b4d4c0e0205bf32cfbd66250cf1f5eab567
fbatisteti <fbatisteti@hotmail.com>
2020-09-28 17:26:41 -0300

    Added CRUD

---
+++ Server/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LearningBlazor.Shared;
+
+namespace LearningBlazor.Server
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) {}
+
+        // this is to be done for each of our tables
+        public DbSet<Good> Goods { get; set; }
+    }
+}
---
+++ Server/Controllers/GoodsController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using LearningBlazor.Server;
+using LearningBlazor.Shared;
+
+namespace LearningBlazor.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GoodsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GoodsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Goods
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Good>>> GetGoods()
+        {
+            return await _context.Goods.ToListAsync();
+        }
+
+        // GET: api/Goods/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Good>> GetGood(int id)
+        {
+            var good = await _context.Goods.FindAsync(id);
+
+            if (good == null)
+            {
+                return NotFound();
+            }
+
+            return good;
+        }
+
+        // PUT: api/Goods/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutGood(int id, Good good)
+        {
+            if (id != good.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(good).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!GoodExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Goods
+        [HttpPost]
+        public async Task<ActionResult<Good>> PostGood(Good good)
+        {
+            _context.Goods.Add(good);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetGood", new { id = good.Id }, good);
+        }
+
+        // DELETE: api/Goods/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Good>> DeleteGood(int id)
+        {
+            var good = await _context.Goods.FindAsync(id);
+            if (good == null)
+            {
+                return NotFound();
+            }
+
+            _context.Goods.Remove(good);
+            await _context.SaveChangesAsync();
+
+            return good;
+        }
+
+        private bool GoodExists(int id)
+        {
+            return _context.Goods.Any(e => e.Id == id);
+        }
+    }
+}
